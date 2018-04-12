056916f4010575f062e46ddeeb57eeb0d793ca60
mynkow <mynkow@gmail.com>
2018-04-12 15:30:47 +0300

    Adds build scripts

--- build-configuration.cmd
+++ build-configuration.cmd
@@ -1,16 +1,4 @@
 @echo off
 
-%FAKE% %NYX% "target=clean" -st
-%FAKE% %NYX% "target=RestoreNugetPackages" -st
-
-IF NOT [%1]==[] (set RELEASE_NUGETKEY="%1")
-IF NOT [%2]==[] (set RELEASE_TARGETSOURCE="%2")
-
-SET SUMMARY="Discovery Abstractions"
-SET DESCRIPTION="Discovery Abstractions"
-
-SET SUMMARY_PHRASEAPP="Consul Discovery"
-SET DESCRIPTION_PHRASEAPP="Consul Discovery"
-
-%FAKE% %NYX% appName=Discovery                       appSummary=%SUMMARY% appDescription=%DESCRIPTION% nugetserver=%NUGET_SOURCE_DEV_PUSH% nugetkey=%RELEASE_NUGETKEY%
-%FAKE% %NYX% appName=Discovery.Consul             appSummary=%SUMMARY_PHRASEAPP% appDescription=%DESCRIPTION_PHRASEAPP% nugetserver=%NUGET_SOURCE_DEV_PUSH% nugetkey=%RELEASE_NUGETKEY%
+@powershell -File .nyx\build.ps1 '--appname=Discovery' '--nugetPackageName=Discovery'
+@powershell -File .nyx\build.ps1 '--appname=Discovery.Consul' '--nugetPackageName=Discovery.Consul'
